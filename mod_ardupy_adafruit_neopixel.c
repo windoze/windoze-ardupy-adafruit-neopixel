4f3c5b570fae465d69e62640fbc13676064e43fb
Chen Xu <windoze@0d0a.com>
2020-05-09 03:36:21 +0800

    WIP

--- mod_ardupy_adafruit_neopixel.c
+++ mod_ardupy_adafruit_neopixel.c
@@ -13,16 +13,17 @@ void common_hal_adafruit_neopixel_show(abstract_module_t * self);
 extern const mp_obj_type_t adafruit_neopixel_type;
 
 m_generic_make(adafruit_neopixel) {
-    enum { ARG_pin,  };
+    enum { ARG_clk, ARG_dat,  };
     static const mp_arg_t allowed_args[] = {
         { MP_QSTR_pin,  MP_ARG_INT ,{.u_int = -1}},
+        { MP_QSTR_num,  MP_ARG_INT ,{.u_int = -1}},
     };
     abstract_module_t * self = new_abstruct_module(type);
     mp_arg_val_t        vals[MP_ARRAY_SIZE(allowed_args)];
-    mp_arg_check_num(n_args, n_kw, 1, 1, false);
+    mp_arg_check_num(n_args, n_kw, 2, 2, false);
     mp_arg_parse_all_kw_array(n_args, n_kw, args, MP_ARRAY_SIZE(allowed_args), allowed_args, vals);
 
-    common_hal_adafruit_neopixel_construct(self, vals[ARG_pin].u_int);
+    common_hal_adafruit_neopixel_construct(self, vals[ARG_pin].u_int, vals[ARG_dat].u_int);
     return self;
 }
 
@@ -41,6 +42,7 @@ mp_obj_t adafruit_neopixel_show(size_t n_args, const mp_obj_t * pos_args, mp_map
     );
     return mp_const_none;
 }
+
 void adafruit_neopixel_obj_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest){
     generic_method_lookup(self_in, attr, dest);
 }
